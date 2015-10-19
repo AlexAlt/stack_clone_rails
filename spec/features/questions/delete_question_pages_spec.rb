51a1b74ef545c91a63c35a79c4c92c5def04d5b4
Alex Altieri & Alyssa Horrocks <student@epicodus.com>
2015-10-19 16:03:59 -0700

    Pass spec for deleting questions

---
+++ spec/features/questions/delete_question_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'the process to delete a question' do
+  it 'deletes a question' do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on 'Log In'
+    fill_in 'Email', :with => 'alyssa@alyssa.com'
+    fill_in 'Password', :with => 'alyssa'
+    click_on 'Log In'
+    click_on 'Ask Question'
+    fill_in "Title", :with => 'What is love?'
+    fill_in "Content/Description", :with => 'Please explain in English'
+    click_on "Submit"
+    click_on 'What is love?'
+    click_on 'Delete'
+    expect(page).to have_content "WELCOME"
+  end
+end
